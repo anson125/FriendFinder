26c70eadbb9eaa15ddfc7e290f90ce4623fe20aa
Ken Tillman <kentillman@Kens-MacBook-Pro.local>
2018-04-02 23:58:38 -0400

    Friend Finder

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Mewtwo',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/7/78/150Mewtwo.png/250px-150Mewtwo.png',
+		scores: [
+            '5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+            '5'
+		]
+	},
+	{
+		name: 'Gyarados',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/4/41/130Gyarados.png/250px-130Gyarados.png',
+		scores: [
+            '3',
+			'1',
+			'2',
+			'1',
+			'4',
+			'2',
+			'2',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Arceus',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/f/fc/493Arceus.png/250px-493Arceus.png',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Gengar',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/c/c6/094Gengar.png/250px-094Gengar.png',
+		scores: [
+            '1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Rayquaza',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/e/e4/384Rayquaza.png/250px-384Rayquaza.png',
+		scores: [
+            '5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+            '4'
+		]
+	},
+	{
+		name: 'Snorlax',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/f/fb/143Snorlax.png/250px-143Snorlax.png',
+		scores: [
+            '5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+            '3'
+		]
+	},
+	{
+		name: 'Squirtle',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png',
+		scores: [
+            '1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+            '1'
+		]
+	},
+	{
+		name: 'Pikachu',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png',
+		scores: [
+            '1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+            '5'
+		]
+	},
+	{
+		name: 'Charmander',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png',
+		scores: [
+            '5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+            '5'
+		]
+	},
+	{
+		name: 'Bulbasaur',
+		photo: 'https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png',
+		scores: [
+            '2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+            '5'
+		]
+	}
+];
+
+module.exports = friendsArray;
